311729aae0f64a3d13cbe97fae5b90c9780a9e1e
sameerasw <sameera.whoami@gmail.com>
2025-02-19 00:39:53 +0530

    Added main menu and about dialog

--- app/src/main/java/com/sameerasw/dicing/MainActivity.kt
+++ app/src/main/java/com/sameerasw/dicing/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sameerasw.dicing.ui.theme.DicingTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +34,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DicingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    DicingTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                TitleBar()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = CenterHorizontally
+                ) {
+                    MainMenu()
                 }
             }
         }
@@ -31,17 +63,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TitleBar(){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Dicing",
+        textAlign = TextAlign.Center,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MainMenu() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("About") },
+            text = {
+                    Text(
+                        "Devloped by Sameera Wijerathna. (20223140)\n" +
+                            "I confirm that I understand what plagiarism is and have read and\n" +
+                            "understood the section on Assessment Offences in the Essential\n" +
+                            "Information for Students. The work that I have submitted is\n" +
+                            "entirely my own. Any work from other authors is duly referenced\n" +
+                            "and acknowledged.",
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+                   },
+            confirmButton = {
+                Button(onClick = { showDialog = false }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
+
+    Column(
+        horizontalAlignment = CenterHorizontally,
+        modifier = Modifier
+            .padding(32.dp).border(width = 2.dp, color = MaterialTheme.colorScheme.primary, shape = MaterialTheme.shapes.medium)
+            .padding(32.dp)
+
+    ) {
+        Text(
+            text = "Main Menu",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.85f),
+            fontWeight = FontWeight.Medium
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { }) {
+            Text(text = "Start Game")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { showDialog = true }) {
+            Text(text = "About")
+        }
+    }
+}
+
+
+@Preview(showBackground = true, device = Devices.PIXEL_6A)
 @Composable
 fun GreetingPreview() {
-    DicingTheme {
-        Greeting("Android")
-    }
+    App()
 }
